e841e6e0ffa118be0eefb4cfca9692758ad2f7a1
Hailin Ruan <hlruan829@gmail.com>
2023-10-06 00:02:29 +1100

    final

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// Add and configure workbox plugins for a service worker and manifest file.
 
 module.exports = (env, argv) => {
   const injectManifest = new InjectManifest({
@@ -36,7 +36,7 @@ module.exports = (env, argv) => {
       // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards',
+        title: 'Text Editor',
       }),
 
       // Injects our custom service worker
@@ -46,9 +46,9 @@ module.exports = (env, argv) => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Text Editor',
+        short_name: 'Text',
+        description: 'Edit texts!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
